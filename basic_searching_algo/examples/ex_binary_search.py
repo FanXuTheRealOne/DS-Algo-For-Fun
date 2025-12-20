43531898f3c08956d1f1cc1da533defbeb9174a5
xufan <fxu9@stevens.edu>
2025-12-20 01:53:16 -0500

    h

--- basic_searching_algo/examples/ex_binary_search.py
+++ basic_searching_algo/examples/ex_binary_search.py
@@ -1,12 +1,13 @@
 from basic_searching_algo.algos import SearchAlgo
-
+import math
 if __name__ == "__main__":
+    print(100 * math.pow(1.1,52))
 
     list = [1,2,3,4,5,6,7,8,888,8889]
     target = 8889
 
     s = SearchAlgo.BinarySearch()
-    # 这里因为 binary Search里有self，所以它是一个跟着object的method，必须要new出来一个新的object才用这个
+
 
     output = s.binary_search(list,target)
 
@@ -14,6 +15,8 @@ if __name__ == "__main__":
         print("Not found")
     else:
         print(f"found {target} at {output}")
+
+
 
 
 
