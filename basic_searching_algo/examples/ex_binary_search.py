381686723a547132a7526e6fd8851bc15aa7b450
xufan <xufan146@gmail.com>
2025-12-26 19:00:12 -0500

    modify

--- basic_searching_algo/examples/ex_binary_search.py
+++ basic_searching_algo/examples/ex_binary_search.py
@@ -1,7 +1,6 @@
 from basic_searching_algo.algos import SearchAlgo
 import math
 if __name__ == "__main__":
-    print(100 * math.pow(1.1,52))
 
     list = [1,2,3,4,5,6,7,8,888,8889]
     target = 8889
